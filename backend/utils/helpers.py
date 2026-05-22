a2611c0455dd3338c6e005929f1705dff26f2509
Kumari Sweety Pandit <146050679+Sweety-Pandit@users.noreply.github.com>
2026-05-22 19:18:45 +0530

    Add files via upload

---
+++ backend/utils/helpers.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+def clean_column_names(df: pd.DataFrame):
+
+    df.columns = (
+        df.columns
+        .str.strip()
+        .str.lower()
+        .str.replace(" ", "_")
+    )
+
+    return df
